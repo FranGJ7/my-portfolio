5a8be84eb71296db47582b5391aecdd8ee363626
Fransérgio Gonçalves Júnior <97806169+FranGJ7@users.noreply.github.com>
2023-07-07 12:49:32 -0300

    Update index.tsx

--- src/section/welcomeSection/index.tsx
+++ src/section/welcomeSection/index.tsx
@@ -8,10 +8,10 @@ export const WelcomeSection = () => {
         <>
             <div className={styles.container}>
                 <div className={styles.titleSection}>
-                    <h2 className={styles.title}>Olá sou o Fransérgio! <br /> Desenvolvedor  Javascript FullStack Web & Mobile</h2>
+                    <h2 className={styles.title}>Olá sou o Fransérgio! <br /> Desenvolvedor  Mobile</h2>
                     <p className={styles.subTitle}>Aqui você vai encontrar meus trabalhos como projetos pessoais e freelancer, incluindo links para visualização dos apps no Expo Go, além do acesso aos sites e códigos fonte. Também foi criada uma linha do tempo para acompanhar minha jornada como desenvolvedor.</p>
                 </div>
-                <a href="https://drive.google.com/file/d/12pPAhlD7yWtvuOV7VrtzN2h8klAqOK_0/view?usp=sharing" target="blank">
+                <a href="https://drive.google.com/file/d/1bKACMXEMBIHwMEl3qXHNsveo4c1bv_dd/view?usp=sharing" target="blank">
                     <button className={styles.button}>Curriculo <BsFileEarmarkArrowDown size={15} /></button>
                 </a>
             </div>
