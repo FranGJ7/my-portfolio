8e9459b5a2bdad1caa65e3097f0663f4682a94dc
Fransérgio Gonçalves Júnior <97806169+FranGJ7@users.noreply.github.com>
2023-08-24 23:26:22 -0300

    Update index.tsx

--- src/section/welcomeSection/index.tsx
+++ src/section/welcomeSection/index.tsx
@@ -8,7 +8,7 @@ export const WelcomeSection = () => {
         <>
             <div className={styles.container}>
                 <div className={styles.titleSection}>
-                    <h2 className={styles.title}>Olá sou o Fransérgio! <br /> Desenvolvedor  Mobile</h2>
+                    <h2 className={styles.title}>Olá sou o Fransérgio! <br /> Desenvolvedor Fullstack.</h2>
                     <p className={styles.subTitle}>Aqui você vai encontrar meus trabalhos como projetos pessoais e freelancer, incluindo links para visualização dos apps no Expo Go, além do acesso aos sites e códigos fonte. Também foi criada uma linha do tempo para acompanhar minha jornada como desenvolvedor.</p>
                 </div>
                 <a href="https://docs.google.com/document/d/11LmOfNqePMHqqWmEwzSFLvOxKPLMM4o0I8h4ZEBilk0/edit?usp=sharing" target="blank">
